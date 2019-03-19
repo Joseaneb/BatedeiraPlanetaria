86dc992388e5290b11b3ad9221a52d9834519bf2
Joseaneb <joseane_bortoli@hotmail.com>
2019-03-19 15:18:34 -0300

    Iniciando repositorio

--- src/main/java/poo/BatedeiraPlanetaria.java
+++ src/main/java/poo/BatedeiraPlanetaria.java
@@ -1,4 +1,72 @@
 package poo;
 
+import java.util.Scanner;
+
 public class BatedeiraPlanetaria {
+
+    //metodos
+    public void aumentarVelocidade() {
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Digite a opção 0 para ligar e 1 para aumentar a velocidade da batedeira: ");
+        int velocidade = teclado.nextInt();
+
+        switch (velocidade) {
+            case 0:
+                velocidade++;  //se estiver desligada passa para ligada
+                break;
+            case 1:
+                velocidade++;  //se estiver na velocidade baixa passa para a velocidade alta
+                break;
+            default:
+                System.out.println("Mantén velocidade!");
+
+            // se estiver na velocidade alta, apenas mantém a velocidade
+
+        }
+    }
+
+    public void diminuirVelocidade() {
+
+        Scanner teclado2 = new Scanner(System.in);
+        System.out.println("Digite a opção 1 para desligar e 2 para diminuir a velocidade da batedeira: ");
+        int velocidade = teclado2.nextInt();
+
+        switch (velocidade) {
+            case 1:
+                velocidade--;  //se estiver na velocidade baixa, passa para desligada
+                break;
+            case 2:
+                velocidade--;  //se estiver na velocidade alta passa para velocidade baixa
+                break;
+            default:
+                System.out.println("Desligado!");
+            // se desligado não faz nada
+        }
+    }
+    public void opcaoBatedores(){
+
+        Scanner teclado3 = new Scanner(System.in);
+        System.out.println("Escolha o batedor:");
+        System.out.println("Opção 1 - Batedor para clara em neve");
+        System.out.println("Opçãi 2 - Batedor para massa leve");
+        System.out.println("Opção 3 - Batedor para massa pesada");
+        int batedor = teclado3.nextInt();
+
+        switch (batedor){
+            case 1:
+                System.out.println("Batedor para clara em neve");
+            break;
+            case 2:
+                System.out.println("Batedor para massa leve");
+            break;
+            case 3:
+                System.out.println("Batedor para massa pesada");
+            break;
+            default:
+                System.out.println("Informe opção para batedores");
+        }
+    }
+
+
 }
--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -1,4 +1,17 @@
 package poo;
 
 public class Principal {
+    public static void main(String[] args) {
+
+        BatedeiraPlanetaria ex1 = new BatedeiraPlanetaria();
+        ex1.aumentarVelocidade();
+
+        BatedeiraPlanetaria ex2 = new BatedeiraPlanetaria();
+        ex2.diminuirVelocidade();
+
+        BatedeiraPlanetaria ex3 =new BatedeiraPlanetaria();
+        ex3.opcaoBatedores();
+
+
+    }
 }
